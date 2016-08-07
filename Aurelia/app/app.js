9e1abc227524c4d69cd20eb9f1c1f0e07e02f52b
rodriguesl3 <l.rsouza@outlook.com>
2016-08-07 12:57:36 +0100

    two-way data binding

--- Aurelia/app/app.js
+++ Aurelia/app/app.js
@@ -2,5 +2,7 @@ export class App{
     constructor(){
         this.message = "Hello Aurelia";
         this.image = "http://aurelia.io/images/main-logo.svg";
+        this.input ="";
+        
     }
 }
